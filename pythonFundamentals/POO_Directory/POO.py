54c3fe49f858ffb88175e777bc39f53c405010c7
CCamiloACastro <cesarcalonsoc@gmail.com>
2023-04-03 00:49:25 -0500

    POO_directory

--- pythonFundamentals/POO_Directory/POO.py
+++ pythonFundamentals/POO_Directory/POO.py
@@ -7,37 +7,42 @@ Se define de esta forma UpperCamelCase
 Instancia: El objeto que es creado al llamar una clase
 Atributo de una clase: Propiedad que tendrían todas las instancias
 Método: Función que existe de una clase
-
-Construnctor: Es una función que se ejecuta automaticamente al crear un nuevo objeto por medio de una clase
 """
 
 
 class Restaurant:
-    # __init__(self): Es un constructor que se ejecuta automaticamente sin tener que llamarlo
+
+    # __init__(self): Es un constructor que se ejecuta automáticamente sin tener que llamarlo
+    # Un constructor es una función única que se llama automáticamente cuando se crea un objeto de una clase.
+    # El objetivo principal de un constructor es inicializar o asignar valores a los miembros de datos de esa clase.
+    # No puede retornar ningún valor que no sea None.
+
     # Cualquier atributo que se agregue en init se tiene que colocar al momento de instanciarlo
-    def __init__(self, category):
-        # None es frecuentemente usado para representar la ausencia de valor
-        self.nombre = None
+    def __init__(self, name, category, price):
+        # None es frecuentemente usado para representar la ausencia de valor (vacío)
+        self.years_business = None
+        self.name = name
         self.category = category
-        print('me ejecuto automáticamente')
+        self.price = price
 
     # self: ofrece un modo de cálculo para hacer referencia al contenido del objeto con el que está asociado sin
     # tener que hacer referencia explícitamente al objeto.
     def add_restaurant(self, nombre):
         print('Agregando...')
-        self.nombre = nombre
-        print(f'Agregado {self.nombre}')
+        self.name = nombre
+        print(f'Agregado')
 
     def show_information(self):
-        print(f'Nombre: {self.nombre}, Categoria: {self.category}')
+        print(f'\t{self.name} \nCategoria: {self.category}\nPrecio: {self.price}')
 
 
 # Instanciar la clase
-restaurant = Restaurant('Comida tolimense')
-restaurant.add_restaurant('Tamales uyuy')
+restaurant = Restaurant('Tamales uyuy','Comida tolimense', 50)
+# print(restaurant.name)
+restaurant.add_restaurant('Tamales uyuyuy')
+# print(restaurant.name)
 
-restaurant2 = Restaurant('Comida rapida')
-restaurant2.add_restaurant('Pizzeria uyuy')
+restaurant2 = Restaurant('Pizzeria uyuyuy','Comida rapida',20)
 
 restaurant.show_information()
 restaurant2.show_information()
